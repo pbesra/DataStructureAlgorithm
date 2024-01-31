e5eb425a92797e4f4c9cb42a27fd89bfe0edaa65
pbesra <psbesra@gmail.com>
2024-01-31 23:32:51 +0530

    graph: AdjacencyMatrix added.

--- src/data-structure/graph.cpp
+++ src/data-structure/graph.cpp
@@ -1,3 +1,10 @@
+
+/*
+This deals with undirected graph.
+There are two data structures here:
+1.) AdjacencyList
+2.) AdjacencyMatrix
+*/
 #include <iostream>
 #include <vector>
 #include <memory>
@@ -24,8 +31,9 @@ public:
 class AdjacencyList
 {
 private:
-public:
     vector<vector<EdgeNode *>> edges;
+
+public:
     AdjacencyList(int numVertices) : edges(numVertices)
     {
     }
@@ -62,9 +70,45 @@ public:
     }
 };
 
+class AdjacencyMatrix
+{
+private:
+    vector<vector<EdgeNode *>> edgeMatrix;
+
+public:
+    AdjacencyMatrix(int numVertices) : edgeMatrix(numVertices, std::vector<EdgeNode *>(numVertices, nullptr)) {}
+    void addEdge(const int src, const int dest)
+    {
+        edgeMatrix[src][dest] = new EdgeNode(dest);
+        edgeMatrix[dest][src] = new EdgeNode(dest);
+    }
+    void removeEdge(const int src, const int dest)
+    {
+        edgeMatrix[src][dest] = nullptr;
+        edgeMatrix[dest][src] = nullptr;
+    }
+
+    void printEdges()
+    {
+        for (int i = 0; i < edgeMatrix.size(); i++)
+        {
+            cout << i << "->";
+            for (int j = 0; j < edgeMatrix[i].size(); j++)
+            {
+                if (edgeMatrix[i][j] != nullptr)
+                {
+                    cout << j << ",";
+                }
+            }
+            cout << endl;
+        }
+    }
+};
+
 int main()
 {
 
+    cout << "------ Start AdjacencyList -------" << endl;
     unique_ptr<AdjacencyList> list = make_unique<AdjacencyList>(4);
     list->addEdge(0, 1);
     list->addEdge(1, 2);
@@ -73,5 +117,14 @@ int main()
     list->addEdge(3, 1);
     list->removeEdge(3, 1);
     list->printAllEdges();
+
+    cout << "------ Start AdjacencyMatrix -------" << endl;
+    unique_ptr<AdjacencyMatrix> matrix = make_unique<AdjacencyMatrix>(4);
+    matrix->addEdge(0, 1);
+    matrix->addEdge(0, 2);
+    matrix->addEdge(0, 3);
+    matrix->addEdge(2, 3);
+    matrix->removeEdge(0, 1);
+    matrix->printEdges();
     return 0;
 }
