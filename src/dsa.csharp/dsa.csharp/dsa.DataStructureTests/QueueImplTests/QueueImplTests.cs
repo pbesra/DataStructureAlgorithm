644a000479bc539a7b4b79ba49331e3f5f4dd2d6
pbesra <psbesra@gmail.com>
2026-03-15 19:29:48 +0530

    Queue test cases added

--- src/dsa.csharp/dsa.csharp/dsa.DataStructureTests/QueueImplTests/QueueImplTests.cs
+++ src/dsa.csharp/dsa.csharp/dsa.DataStructureTests/QueueImplTests/QueueImplTests.cs
@@ -31,6 +31,54 @@ namespace dsa.DataStructureTests.QueueImplTests
              Assert.Equal(testData[0], dequeueData1);
              Assert.Equal(testData[1], dequeueData2);
         }
+        
+        [Fact]
+        public void TestEnqueueAndPeek()
+        {
+            //Arrange
+            var testData = new List<int> { 10, 21, 13, 24, 45, 79, 14, 57, 19, 17 };
+            //Act
+            foreach (var item in testData)
+            {
+                _queueInt.Enqueue(item);
+            }
+            var dequeueData1 = _queueInt.Dequeue(); // 1
+            var dequeueData2 = _queueInt.Dequeue(); // 2
+            
+            var peekItem = _queueInt.Peek();
+
+            Assert.Equal(testData[2], peekItem);
+        }
+        
+        [Fact]
+        public void TestEnqueueAndClear()
+        {
+            //Arrange
+            var testData = new List<int> { 10, 21, 13, 24, 45, 79, 14, 57, 19, 17 };
+            //Act
+            foreach (var item in testData)
+            {
+                _queueInt.Enqueue(item);
+            }
+            _queueInt.Clear();
+            int size = _queueInt.Size();
+            Assert.Equal(0, size);
+        }
+        
+        [Fact]
+        public void TestEnqueueAndIsEmpty()
+        {
+            //Arrange
+            var testData = new List<int> { 10, 21, 13, 24, 45, 79, 14, 57, 19, 17 };
+            //Act
+            foreach (var item in testData)
+            {
+                _queueInt.Enqueue(item);
+            }
+            _queueInt.Clear();
+            bool isEmpty = _queueInt.IsEmpty();
+            Assert.True(isEmpty);
+        }
     }
 }
         
