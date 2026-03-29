08d8008083a7a7ffc3ede83c9a89ee76cc35d54c
Besra <I43909@verisk.com>
2026-03-30 00:32:46 +0530

    Trie ds added

--- src/dsa.csharp/dsa.csharp/dsa.DataStructure/Trees/TrieTree.cs
+++ src/dsa.csharp/dsa.csharp/dsa.DataStructure/Trees/TrieTree.cs
@@ -2,5 +2,37 @@ namespace dsa.DataStructure.Trees;
 
 public class TrieTree
 {
-    
+    private TrieNode _root;
+
+    public void Add(string word)
+    {
+        if (string.IsNullOrEmpty(word))
+        {
+            return;
+        }
+        var currentNode = _root;
+        if (currentNode == null)
+        {
+            currentNode = new TrieNode(word[0]);
+             _root = currentNode;
+        }
+        foreach (var ch in word)
+        {
+            
+        }
+    }
+}
+
+public class TrieNode
+{
+    public char Value { get; set; }
+    public Dictionary<char, TrieNode> Children { get; set; }
+    public bool IsEndOfWord { get; set; }
+
+    public TrieNode(char value)
+    {
+        Value = value;
+        Children = new Dictionary<char, TrieNode>();
+        IsEndOfWord = false;
+    }
 }
